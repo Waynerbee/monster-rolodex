56dabf4ce3c81511c7e64d766dd5cfcc8285d313
wayne wee <wayne9wee@hotmail.com>
2020-07-27 18:01:57 +0800

    Updates

---
+++ precache-manifest.2f22a25a7b4ae9169e9c5d1fc5aba779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5401e3f1efca50a248348c122b68fb76",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "53a5b06fedd04907f4f7",
+    "url": "/monster-rolodex/static/css/main.de2a86f8.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monster-rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a5b06fedd04907f4f7",
+    "url": "/monster-rolodex/static/js/main.74397fc0.chunk.js"
+  },
+  {
+    "revision": "98c8b3d0c3ab21abc082",
+    "url": "/monster-rolodex/static/js/runtime-main.9ae1052e.js"
+  }
+]);
